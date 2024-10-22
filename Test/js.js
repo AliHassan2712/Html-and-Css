eb6b0de7914d01fee3cffd8c2af03353e28f0bf0
alihassan2712 <alohassan05999@gmail.com>
2024-10-22 17:52:26 +0300

    Basic Js

---
+++ Test/js.js
@@ -0,0 +1,107 @@
+/********************Basic Js ************/
+/*js java script language 'loosely type languge'
+tag scripte in last body 'child'
+
+output :
+document.write()
+console.log()
+window.alert()
+
+console is API not fun Js 
+
+ES6 'Orginatin response to put standered info like as language program like as Js '
+
+
+
+data type : 
+String
+Numbers
+Array => Object  []
+obj => Object  {}
+boolean
+undefined
+null => Object
+
+
+
+Variables :
+CamalCase sensitive
+( _  $ ali1) ✓
+anything (1ali +ali )×
+
+
+HTML <a id=''>
+   id is Variable   //Important
+</a>
+
+                var          let       const 
+         
+redeclar:         ✓            ×          ×
+Access befor:   undifine       ×          ×
+Scope Drama:  not add to window ×         ×
+block fun scope ({}):
+
+*/
+
+
+
+// Test Code ' Domهنستخدمه ل قدام كثير في '
+/*tittle = 'Ali'
+parg = 'AliHassan'
+time = '25/10'
+markup = (`
+<div>
+  <h1>
+    Hello ${tittle}
+  </h1>
+    <p>
+      Your is ${parg}
+    </p>
+    <span>
+     ${time}
+    </span>
+</div>
+`)
+
+function repeat(fun,count) {
+  for (var i = 0; i < count; i++) {
+    document.write(fun);
+    document.write('-----------------')
+  }
+}
+repeat(markup,4); */
+//document.write(markup.repeat(4))
+
+
+
+/*******************Numbers***************/
+/*
+console.log(1_000_000);
+console.log((100).toString())
+console.log(100..toString())
+console.log((100.55556).toFixed(3))
+console.log(parseInt('15 alihassan0'))
+console.log(Number.isInteger('100'))
+console.log(Number.isNaN(false))
+*/
+
+//round() ceil() floor() min() max() tranc() 
+
+
+/*********************String***************/
+/*name = 'Ali Hassan Amer Abu Safiha';
+console.log(name[5])
+console.log(name.charAt(5))
+console.log(name.trim())
+console.log(name.trim().charAt(5).toUpperCase())
+console.log(name.indexOf('m'))
+console.log(name.lastIndexOf('f'))
+console.log(name.slice(2,7))
+console.log(name.slice(-2))
+console.log(name.split(' ') , 2);
+console.log(name.substring(7,2))
+console.log(name.includes('m',15))
+console.log(name.startwith('m',15))
+console.log(name.endWith('m',15))
+*/
+
